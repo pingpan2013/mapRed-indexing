289afee4192166d5d9d835b8257135391931943e
pingpan <pingpan@umich.edu>
2014-12-04 16:01:14 -0500

    step 3

--- mapred_indexer/run_inv.sh
+++ mapred_indexer/run_inv.sh
@@ -3,12 +3,16 @@
 export JAVA_HOME=/usr
 
 # pipeline step one 
-rm -rf output1
-../bin/hadoop jar ./dist/InvertedIndexer.jar edu.umich.cse.eecs485.InvertedIndex1 ../dataset/test output1
+#rm -rf output1
+#../bin/hadoop jar ./dist/InvertedIndexer.jar edu.umich.cse.eecs485.InvertedIndex1 ../dataset/test output1
 
 # pipeline step two 
-rm -rf output2
-../bin/hadoop jar ./dist/InvertedIndexer.jar edu.umich.cse.eecs485.InvertedIndex2 output1 output2
+#rm -rf output2
+#../bin/hadoop jar ./dist/InvertedIndexer.jar edu.umich.cse.eecs485.InvertedIndex2 output1 output2
+
+# pipeline step three 
+rm -rf output3
+../bin/hadoop jar ./dist/InvertedIndexer.jar edu.umich.cse.eecs485.InvertedIndex3 output2 output3
 
 
 
--- mapred_indexer/src/InvertedIndex3.java
+++ mapred_indexer/src/InvertedIndex3.java
@@ -1,66 +1,112 @@
 package edu.umich.cse.eecs485;
 
 import java.io.IOException;
-import java.util.*;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.conf.*;
-import org.apache.hadoop.io.*;
-import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import java.util.*;
 
-public class InvertedIndex2 {
-        public static HashSet<String> docsSet = new HashSet<String>();
-
-	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
-		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-			String line = value.toString();
-                        String[] str1 = line.split("\\s+");
-                        String term = str1[0];
-
-                        String[] str2 = str1[1].split(";");
-                        String df = str2[0];
-                        String[] docTotf = str2[1].split(",");
-                        
-                        for()
-
-
-                        // Reorganize using docid as the key
-                        context.write(term, new Text(docid + ":" + cnt));
-                }
-	} 
-
-	public static class Reduce extends Reducer<Text, Text, Text, Text> {
-		public void reduce(Text key, Iterable<Text> values, Context context) 
-			throws IOException, InterruptedException {
-		
+public class InvertedIndex3 {
+    public static HashSet<String> docsSet = new HashSet<String>();    
+    public static ArrayList<Text> cache = new ArrayList<Text>();
+    
+    public static class Map extends Mapper<LongWritable, Text, Text, Text> {
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException { 
+            
+            // split into parts
+            String line = value.toString();
+            String[] str1 = line.split("\\s+");
+            String[] str2 = str1[1].split(";");
+            String term = str1[0];
+            String df = str2[0];
+            String list = str2[1];
+            
+            String[] docs = list.split(",");
+            String[] id_tf;
+            String docid;
+            String tf;
+            for (int i=0; i<docs.length; i++) {
+                id_tf = docs[i].split(":"); 
+                docid = id_tf[0];
+                tf = id_tf[1];
                 
+                // reorganize using docid as the key
+                context.write(new Text(docid), new Text(String.format("%s:%s:%s", term, df, tf)));
+                docsSet.add(docid);
+            }
+        }
+    }
+
+    public static class Reduce extends Reducer<Text, Text, Text, Text> {
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            String term, df, tf;
+            double tfD, dfD, idf, tfidf;
+            double normFac = 1.00;
+            String[] str;
+           
+            int totalDocs = docsSet.size();
+            System.out.println("XXXXXXXX:" + totalDocs );
+
+            for(Text value : values){
+                str = value.toString().split(":");                    
+                term = str[0];
+                dfD = Double.parseDouble(str[1]);
+                tfD = Double.parseDouble(str[2]);
+                normFac += Math.pow(tfD, 2) * Math.pow(Math.log10(totalDocs / dfD), 2);      
+                cache.add(value);
+            }
+
+            for(Text value : cache){
+                System.out.println(value.toString());
+            }
+        
+            StringBuilder list = new StringBuilder(); 
+            for (Text value : cache) {
+                str = value.toString().split(":");
+                term = str[0];
+                df = str[1];
+                tf = str[2];
                 
-                }
-	}
+                tfD = Double.parseDouble(tf);
+                dfD = Double.parseDouble(df);
+                idf = Math.log10(totalDocs / dfD);
+                tfidf = tfD * idf;
+                list.append(String.format("%s:%s:%f,", term, df, tfidf));
+            }
+    
+            // remove trailing coma 
+            list.deleteCharAt(list.length()-1);
+            context.write(key, new Text(list.toString()));
+        }
+    }
 
-	public static void main(String[] args) throws Exception {
-		Configuration conf = new Configuration();
+    public static void main(String[] args) throws Exception
+    {
+        Configuration conf = new Configuration();
 
-		Job job = new Job(conf, "InvertedIndex2");
+        Job job = new Job(conf, "InvertedIndex3");
 
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
 
-		job.setMapperClass(Map.class);
-		job.setReducerClass(Reduce.class);
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
 
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
 
-		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-		job.waitForCompletion(true);
-	}
-
+        job.waitForCompletion(true);
+    }
 }
